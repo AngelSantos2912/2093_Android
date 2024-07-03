6753129b6de9db2625e3a4790fa9ec1ed8264b31
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-07-02 22:49:47 -0500

    detalles y mas detalles

--- app/src/main/java/com/example/a2093/ui/Adapter/EventoVH.kt
+++ app/src/main/java/com/example/a2093/ui/Adapter/EventoVH.kt
@@ -7,8 +7,8 @@ import com.example.a2093.databinding.ItemEventoBinding
 class EventoVH(private val binding: ItemEventoBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun agregarItem(evento: Evento){
-        binding.lblevento.text = "Evento : ${evento.organizador}"
+        binding.lblevento.text = "Fecha : ${evento.organizador}"
         binding.lblrazon.text = "Razon : ${evento.razon}"
-        binding.lblfecha.text = "Fecha : ${evento.fecha}"
+        binding.lblfecha.text = "Organizador : ${evento.fecha}"
    }
 }
